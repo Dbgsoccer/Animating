227230b3df89b3443b49e8d287e6abc4a408c846
dbgsoccer <derek.guerrette@maine.edu>
2016-11-01 11:48:26 -0400

    corgi has been added and we are able to touch the screen dimensions

--- app/src/main/java/com/example/chrisbennett/animating/CustomView.java
+++ app/src/main/java/com/example/chrisbennett/animating/CustomView.java
@@ -185,7 +185,7 @@ public class CustomView extends SurfaceView implements SurfaceHolder.Callback {
             view = v;
             time = System.currentTimeMillis();
         }
-
+// fdsfds
 
 
         void setRunning(boolean r) {
