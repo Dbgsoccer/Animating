b50cfb85970ff2dd2f6ee7299e741059cc477f14
dbgsoccer <derek.guerrette@maine.edu>
2016-11-03 11:51:17 -0400

    corgi has been added and we are able to touch the screen dimensions

--- app/src/main/java/com/example/chrisbennett/animating/MainActivity.java
+++ app/src/main/java/com/example/chrisbennett/animating/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chrisbennett.animating;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
     }
+
 }
---
+++ app/src/main/java/com/example/chrisbennett/animating/gameover.java
@@ -0,0 +1,8 @@
+package com.example.chrisbennett.animating;
+
+import android.os.Bundle;
+
+/**
+ * Created by Derek on 10/29/2016.
+ */
+
